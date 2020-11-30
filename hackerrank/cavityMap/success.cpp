c2f72f359e5c781a20b748bf0b5a853735bc1e4a
Mohammed Aminu <aminuolawalekan@gmail.com>
2020-11-30 21:46:21 +0100

    solved cavityMap

---
+++ hackerrank/cavityMap/success.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+    return 0;
+}
+
+
+vector<string> cavityMap(vector<string> grid) {
+    for(unsigned int i=1; i < grid.size()-1;i++){
+        for(unsigned int j=1; j< grid.size()-1; j++){
+            int c = grid[i][j];
+            int f = grid[i][j+1];
+            int b = grid[i][j-1];
+            int bt = grid[i+1][j];
+            int tp = grid[i-1][j];
+            if(c>f && c > b && c > tp && c > bt){
+                grid[i][j] = 'X';
+            }
+        }
+    }
+    return grid;
+
+}
